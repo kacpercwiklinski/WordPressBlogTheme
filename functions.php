ba4a935f4436c5478d89e3773a5e94f5ba0d07b8
kacpercwiklinski <kacper.cwiklinski9@gmail.com>
2022-05-22 19:45:10 +0200

    Added oswald font

--- functions.php
+++ functions.php
@@ -5,8 +5,9 @@ function register_styles(){
 
     wp_enqueue_style('wordpressblog-style', get_template_directory_uri() . "/style.css", array('wordpressblog-bootstrap-min'), $version, 'all');
     wp_enqueue_style('wordpressblog-bootstrap-min', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.3', 'all');
+    wp_enqueue_style('wordpressblog-bootstrap-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array(), '5.0.2', 'all');
     wp_enqueue_style('wordpressblog-fontawsome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css', '6.1.1', array(), 'all');
-    wp_enqueue_style('wordpressblog-fonts-googleapis', 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap', false, array(), 'all');
+    wp_enqueue_style('wordpressblog-fonts-googleapis', 'https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&family=Roboto:wght@100;400&display=swap', false, array(), 'all');
 }
 
 add_action( 'wp_enqueue_scripts', 'register_styles');
